d8738018ef53a6d46993fe2a31f6094ae6a24d07
Aleh Karpuk <oleg.karpuk.1982@gmail.com>
2024-04-07 20:35:10 +0300

    Homework 5 added

---
+++ homework/aleh_karpuk/Homework_5/Task_3.py
@@ -0,0 +1,7 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+student_1, student_2, student_3 = students
+subject_1, subject_2, subject_3 = subjects
+
+print(f"Students {student_1}, {student_2}, {student_3} study these subjects: {subject_1}, {subject_2}, {subject_3}")
