b4467881a07bb0fbdf587269e97791e7f0b77fef
Aleh Karpuk <oleg.karpuk.1982@gmail.com>
2024-04-09 08:45:12 +0300

    Homework 6 - Task 1 added

---
+++ homework/aleh_karpuk/Homework_6/Task_1.py
@@ -0,0 +1,20 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+
+words = text.split()
+fin_words = []
+
+for word in words:
+    if word[-1] in {','}:
+        word = word.replace(',', 'ing, ')
+        fin_words.append(word)
+
+    elif word[-1] in {'.'}:
+        word = word.replace('.', 'ing. ')
+        fin_words.append(word)
+
+    else:
+        word = word + 'ing '
+        fin_words.append(word)
+
+print(' '.join(fin_words))
+
