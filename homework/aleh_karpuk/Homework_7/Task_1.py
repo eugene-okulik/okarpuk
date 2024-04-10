fadd3609e00da598ed282b75ac2f70344d6cb396
okarpuk <oleg.karpuk.1982@gmail.com>
2024-04-10 21:19:11 +0300

    Homework 7 - task 1 added

---
+++ homework/aleh_karpuk/Homework_7/Task_1.py
@@ -0,0 +1,12 @@
+secret_digit = 10
+
+while True:
+    user_input = int(input('Try to get secret digit: '))
+    if user_input == secret_digit:
+        break
+
+    elif user_input != secret_digit:
+        print('Sorry, but no...')
+        continue
+
+print('Got it! You win!')
