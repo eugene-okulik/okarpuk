0ef7951cd08fd7a35bf63ab6cedbac3fe5fd48c4
okarpuk <oleg.karpuk.1982@gmail.com>
2024-04-16 08:54:27 +0300

    Homework 8 - task 2 - syntax corrections

--- homework/aleh_karpuk/Homework_8/Task_2.py
+++ homework/aleh_karpuk/Homework_8/Task_2.py
@@ -1,11 +1,12 @@
 def fibonacci(n):
     first_digit = 0
     second_digit = 1
-    for i in range(n-1):
+    for i in range(n - 1):
         temp_digit = second_digit
         second_digit = first_digit + second_digit
         first_digit = temp_digit
     print(first_digit)
+
 
 fibonacci(5)
 fibonacci(200)
