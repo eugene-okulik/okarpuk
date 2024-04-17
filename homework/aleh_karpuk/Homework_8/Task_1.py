cde3418c927ccbb4a13f0f3841f4f65b9c24b748
Aleh Karpuk <oleg.karpuk.1982@gmail.com>
2024-04-17 23:54:51 +0300

    Homework 8 - task 1 minor changes

--- homework/aleh_karpuk/Homework_8/Task_1.py
+++ homework/aleh_karpuk/Homework_8/Task_1.py
@@ -8,7 +8,7 @@ def salary_sum(salary):
     if bonus is True:
         print(f'You have a bonus - {random_bonus}. Your salary with bonus is - {salary + random_bonus}')
     else:
-        print(f'No bonus this month. Your salary is - {salary}')
+        print(f'There is no bonus this month. Your salary is - {salary}')
 
 
 salary_sum(int(input('Enter your salary ')))
