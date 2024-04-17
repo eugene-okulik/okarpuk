050a0c244e2cf50101a7fe7b80dab1318fd4cff9
Aleh Karpuk <oleg.karpuk.1982@gmail.com>
2024-04-17 19:02:50 +0300

    Homework 8 - task 1 codestyle change

--- homework/aleh_karpuk/Homework_8/Task_1.py
+++ homework/aleh_karpuk/Homework_8/Task_1.py
@@ -5,7 +5,7 @@ def salary_sum(salary):
     bonus = bool(random.randint(0, 1))
     random_bonus = random.randrange(1, 10000)
 
-    if bonus == True:
+    if bonus is True:
         print(f'You have a bonus - {random_bonus}. Your salary with bonus is - {salary + random_bonus}')
     else:
         print(f'No bonus this month. Your salary is - {salary}')
