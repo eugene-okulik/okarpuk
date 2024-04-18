fd30c4341c034f32b89628e6052770df26a0852d
okarpuk <oleg.karpuk.1982@gmail.com>
2024-04-18 14:38:10 +0300

    Homework 8 - task 2 rewrited

--- homework/aleh_karpuk/Homework_8/Task_2.py
+++ homework/aleh_karpuk/Homework_8/Task_2.py
@@ -1,14 +1,17 @@
-def fibonacci(n):
-    first_digit = 0
-    second_digit = 1
-    for i in range(n - 1):
-        temp_digit = second_digit
-        second_digit = first_digit + second_digit
-        first_digit = temp_digit
-    print(first_digit)
+def fibonacci():
+    a, b = 0, 1
+    yield a
+    yield b
 
+    while True:
+        a, b = b, a + b
+        yield b
 
-fibonacci(5)
-fibonacci(200)
-fibonacci(1000)
-fibonacci(100000)
+count = 1
+
+for num in fibonacci():
+    if count in [5, 200, 1000, 100000]:
+        print(f'{count} number in Fibonacci sequence: {num}')
+        if count == 100000:
+            break
+    count += 1
