664dbd8c0a6e9d86714058381a97bbdab20b4864
Aleh Karpuk <oleg.karpuk.1982@gmail.com>
2024-04-28 18:59:34 +0300

    Homework 10 - task 2 added

---
+++ homework/aleh_karpuk/Homework_10/Task_2.py
@@ -0,0 +1,13 @@
+def repeat_me(func):
+    def decorator(*args, **kwargs):
+        count = kwargs.get('count', 1)
+        for i in range(count):
+            func(*args)
+    return decorator
+
+
+@repeat_me
+def example(text):
+    print(text)
+
+example('print me', count=5)
