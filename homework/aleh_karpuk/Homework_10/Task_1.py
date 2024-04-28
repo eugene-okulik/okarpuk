bcc3873398ca5d9b3dc3be8a433a0509cf9ea4ea
Aleh Karpuk <oleg.karpuk.1982@gmail.com>
2024-04-28 18:49:54 +0300

    Homework 10 - task 1 added

---
+++ homework/aleh_karpuk/Homework_10/Task_1.py
@@ -0,0 +1,13 @@
+def finish_me(func):
+    def wrapper(*args):
+        result = func(*args)
+        print("finished")
+        return result
+    return wrapper
+
+
+@finish_me
+def example(text):
+    print(text)
+
+example('print me')
