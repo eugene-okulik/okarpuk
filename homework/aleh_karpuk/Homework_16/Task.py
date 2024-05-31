d6ced82020299867850b8866599fda490ea97a99
okarpuk <oleg.karpuk.1982@gmail.com>
2024-05-31 20:34:23 +0300

    Homework 16 - codestyle 2

--- homework/aleh_karpuk/Homework_16/Task.py
+++ homework/aleh_karpuk/Homework_16/Task.py
@@ -26,7 +26,7 @@ with open(hw16_file_path, newline='') as file:
     for row in file_data:
         name, second_name, group_title, book_title, subject_title, lesson_title, mark_value = row
 
-        select_all_info_query = '''SELECT 
+        select_all_info_query = '''SELECT
         students.name,
         students.second_name,
         groups.title,
