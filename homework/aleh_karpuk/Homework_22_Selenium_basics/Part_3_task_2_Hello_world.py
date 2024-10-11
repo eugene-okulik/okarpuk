79beefb5cb9a7f06e0ef81d439ff9f6c5a64ac01
oleg <oleg.karpuk.1982@gmail.com>
2024-10-11 23:03:03 +0300

    Part 3_2 added

---
+++ homework/aleh_karpuk/Homework_22_Selenium_basics/Part_3_task_2_Hello_world.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+import pytest
+
+
+@pytest.fixture()
+def driver():
+    chrome_driver = webdriver.Chrome()
+    chrome_driver.maximize_window()
+    return chrome_driver
+
+
+def test_validate_text(driver):
+    driver.get('https://the-internet.herokuapp.com/dynamic_loading/2')
+    start_button = driver.find_element(By.XPATH, '//div[@id="start"]/button')
+    start_button.click()
+    expected_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//div[@id="finish"]/h4')))
+    expected_text = expected_element.text
+    assert expected_text == 'Hello World!'
