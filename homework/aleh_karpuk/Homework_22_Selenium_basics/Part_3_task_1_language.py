8b482c4c7bfd93dd66673b04e321936fa9ebe78c
oleg <oleg.karpuk.1982@gmail.com>
2024-10-11 21:48:55 +0300

    Part 3_1 added

--- homework/aleh_karpuk/Homework_22_Selenium_basics/Part_3_task_1_language.py
+++ homework/aleh_karpuk/Homework_22_Selenium_basics/Part_3_task_1_language.py
@@ -13,16 +13,12 @@ def driver():
 
 def test_validate_text(driver):
     driver.get('https://www.qa-practice.com/elements/select/single_select')
-
     select_language_field = driver.find_element(By.XPATH, '//select[@name="choose_language"]')
     submit_button = driver.find_element(By.XPATH, '//input[@name="submit"]')
-
     dropdown = Select(select_language_field)
     dropdown.select_by_value('5')
     selected_option_text = dropdown.first_selected_option.text
     submit_button.click()
-
     output_result = driver.find_element(By.XPATH, '//p[@id="result-text"]')
     output_text = output_result.text
-
     assert selected_option_text == output_text, f'Text invalid. Expected {selected_option_text}, but got {output_text}'
