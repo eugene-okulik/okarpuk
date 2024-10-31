c002c27851813e52616fec44786d4be078393745
oleg <oleg.karpuk.1982@gmail.com>
2024-10-31 23:07:15 +0300

    Task 1 completed

--- homework/aleh_karpuk/Homework_27_Playwright_basics_2/task_1.py
+++ homework/aleh_karpuk/Homework_27_Playwright_basics_2/task_1.py
@@ -5,18 +5,8 @@ from time import sleep
 def test_enabled_and_select(page: Page):
     def accept_alert(alert: Dialog):
         alert.accept()
-
     page.on('dialog', accept_alert)
     page.goto('https://www.qa-practice.com/elements/alert/confirm')
     page.locator('.a-button').click()
-
     result_locator = page.locator('#result-text')
     expect(result_locator).to_have_text('Ok')
-
-
-
-
-
-
-
-
