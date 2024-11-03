60b3e03d7fb221310048cde6d9c716130c9c53e1
oleg <oleg.karpuk.1982@gmail.com>
2024-11-03 17:29:28 +0300

    Partially created

--- homework/aleh_karpuk/Homework_28_Playwright_network/task.py
+++ homework/aleh_karpuk/Homework_28_Playwright_network/task.py
@@ -1,10 +1,9 @@
 from playwright.sync_api import Page, expect, Request, Route, APIResponse
 import re
 import json
-from time import sleep
+
 
 def test_iphone_title_interception(page: Page):
-
     def modify_product_name(route: Route):
         response = route.fetch()
         body = response.json()
@@ -13,20 +12,8 @@ def test_iphone_title_interception(page: Page):
         route.fulfill(
             response=response,
             body=body)
-
-    # https://www.apple.com/shop/api/digital-mat?path=library/step0_iphone/digitalmat
     page.route(re.compile('digitalmat$'), modify_product_name)
-
-
     page.goto('https://www.apple.com/shop/buy-iphone')
     page.get_by_role('button', name='Take a closer look - iPhone 16 Pro & iPhone 16 Pro Max').click()
-    sleep(5)
-
-    popup = page.get_by_role("heading", name='яблокофон 16 про', exact=True)
-
-
-    # popup = page.locator('.rf-digitalmat-inlinetabnav-contentsection')
-
-
-    expect(popup).to_have_text('яблокофон 16 про')
-
+    popup_header = page.locator("//div[@id='panel-:r21:-0']//h2[@id='rf-digitalmat-overlay-label-0']")
+    expect(popup_header).to_have_text('яблокофон 16 про')
