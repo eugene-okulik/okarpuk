75e440f198a7a25ef36c4cbc401788116b18bb25
oleg <oleg.karpuk.1982@gmail.com>
2024-11-03 14:54:23 +0300

    Partially created

--- homework/aleh_karpuk/Homework_28_Playwright_network/task.py
+++ homework/aleh_karpuk/Homework_28_Playwright_network/task.py
@@ -5,15 +5,17 @@ from time import sleep
 
 def test_iphone_title_interception(page: Page):
 
-
     def modify_product_name(route: Route):
         response = route.fetch()
         body = response.json()
-        # body['partNumber'] = 'яблокофон 16 про'
 
-        for product in body.get('partNumber', []):
-            if product.get('partNumber') == "iPhone 16 Pro":
-                product['partNumber'] = "яблокофон 16 про"
+
+        body['partNumber'] = 'яблокофон 16 про'
+
+        # for product in body.get('partNumber', []):
+        #     if product.get('partNumber') == "iPhone 16 Pro":
+        #         product['partNumber'] = "яблокофон 16 про"
+        #         break
 
 
         body = json.dumps(body)
@@ -21,21 +23,14 @@ def test_iphone_title_interception(page: Page):
             response=response,
             body=body)
 
+    # https://www.apple.com/shop/api/digital-mat?path=library/step0_iphone/digitalmat
+    page.route(re.compile('/digital-mat$'), modify_product_name)
 
-
-
-
-
-
-    page.route(re.compile('/step0_iphone/'), modify_product_name)
 
     page.goto('https://www.apple.com/shop/buy-iphone')
-
     page.get_by_role('button', name='Take a closer look - iPhone 16 Pro & iPhone 16 Pro Max').click()
     sleep(5)
+
     popup = page.get_by_role("heading", name='iPhone 16 Pro', exact=True)
-
-    print(page.title())
-
     expect(popup).to_have_text('яблокофон 16 про')
 
