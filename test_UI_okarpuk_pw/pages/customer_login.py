45cd1fd6c71a19e7613d8db6420ae8a8733dcb92
oleg <oleg.karpuk.1982@gmail.com>
2024-11-08 19:56:32 +0300

    issue resolved

--- test_UI_okarpuk_pw/pages/customer_login.py
+++ test_UI_okarpuk_pw/pages/customer_login.py
@@ -6,8 +6,8 @@ from test_UI_okarpuk_pw.pages.locators import customer_login_locators as loc
 
 class CustomerLogin(BasePage):
 
-    def check_customer_login_page_name(self):
-        expect(self.find_element(loc.customer_login_page_name_loc)).to_have_text("Customer Login")
+    def check_customer_login_page_name(self, expected_page_name):
+        expect(self.find_element(loc.customer_login_page_name_loc)).to_have_text(expected_page_name)
 
     def check_must_login_alert_text(self, expected_text):
         alert_message = self.page.locator(loc.alert_message_loc).first
